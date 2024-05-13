b47f27d084791a32a311314e35325896d8ad70e0
Dana Guil <hil.dana2904@gmail.com>
2024-05-13 13:04:29 +0300

    Code style

--- app/src/main/java/com/example/pexelsapp/presentation/adapters/diffutil/NetworkPhotoDiffCallback.kt
+++ app/src/main/java/com/example/pexelsapp/presentation/adapters/diffutil/NetworkPhotoDiffCallback.kt
@@ -3,6 +3,7 @@ package com.example.pexelsapp.presentation.adapters.diffutil
 import androidx.recyclerview.widget.DiffUtil
 import com.example.pexelsapp.data.models.NetworkPhoto
 class NetworkPhotoDiffCallback : DiffUtil.ItemCallback<NetworkPhoto>() {
+    
     override fun areItemsTheSame(oldItem: NetworkPhoto, newItem: NetworkPhoto): Boolean {
         return oldItem.id == newItem.id
     }
